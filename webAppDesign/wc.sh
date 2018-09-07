fb39e233bdef57389c7851024aed02d7d506e985
fungleo <web@fengcms.com>
2018-09-07 21:56:29 +0800

    modified:   webAppDesign/1.32.md

--- webAppDesign/wc.sh
+++ webAppDesign/wc.sh
@@ -4,10 +4,10 @@ files=$(ls)
 all=0
 for i in $files; do
   if [ $i != 'wc.sh' ]; then
-    head -n1 $i
+    title=$(head -n1 $i)
     t=$(wc -m $i | sed 's/^[ \t]*//g' | cut -d ' ' -f1)
     all=$(($all+$t))
-    echo '    统计字数: '$t
+    echo $title'    统计字数: '$t
   fi
 done
 
